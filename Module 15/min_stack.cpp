05a49eecc4da188d0dba62105af240f2ce72eb02
Mahmud Pias <mahmudpias9@gmail.com>
2025-01-05 01:13:29 +0600

    Module 15 added

---
+++ Module 15/min_stack.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+    stack<int> st, min_st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(min_st.empty())
+            min_st.push(val);
+        else if(min_st.top() >= val)
+            min_st.push(val);
+    }
+    
+    void pop() {
+        if(st.top() == min_st.top())
+            min_st.pop();
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min_st.top();
+    }
+};
